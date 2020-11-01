43244d2c4897dd7508f8cc96582076e322fe6808
XuaTheGrate <17090652+XuaTheGrate@users.noreply.github.com>
2020-11-02 11:32:48 +1300

    flesh out the cog!

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# invites
-- What is invites? What does it do?
+# Invites
+### What is Invites? What does it do?
+Invites is a small, easy to use cog for tracking which invites members use to join!
 
 - Requirements
-    - D.py (obviously) 1.5.0+
-    - Invites and Guilds intent
+    - discord.py 1.5.0+
+    - GUILDS, SERVER_MEMBERS and INVITES intents
     - `manage_guild` and `manage_channels` permissions
--- invites/cog.py
+++ invites/cog.py
@@ -37,53 +37,61 @@ class Invites(commands.Cog):
         await self.bot.wait_until_ready()
 
         for guild in self.bot.guilds:
-            invites = await guild.invites()
-
-            if invites:
-                self.cache[guild.id] = invites
+            self.cache[guild.id] = await self.fetch_invites(guild) or {}
 
     # would this ever be used by end-users?
     def get_invite(self, code: str):
         for invites in self.cache.values():
-            invite_found = discord.utils.get(invites, code=code)
-
-            if invite_found:
-                return invite_found
+            find = invites.get(code)
+            if find:
+                return find
         return None
 
     def get_invites(self, guild_id: int):
         return self.cache.get(guild_id, None)
 
+    @commands.command()
+    async def invite_stats(self, ctx):
+        import json
+        cache = {g: {i.code: i.uses for i in invs.values()} for g, invs in self.cache.items()}
+        await ctx.send(json.dumps(cache))
+
+    @commands.Cog.listener()
     async def on_invite_create(self, invite):
         # (maybe) send event here
-        cached = self.get_invites(invite.guild.id)
+        print("created invite {invite} in {invite.guild}")
+        cached = self.cache[invite.guild.id]
 
         if cached:
-            cached.append(invite)
+            cached[invite.code] = invite
 
+    @commands.Cog.listener()
     async def on_invite_delete(self, invite):
         # (maybe) send event here
         entry_found = self.get_invites(invite.guild.id)
 
         if entry_found:
-            invite_found = discord.utils.get(entry_found, code=invite.code)
-
-            if invite_found:
-                try:
-                    entry_found.remove(invite_found)
-                except ValueError:
-                    # retained for debugging purposes? maybe add to
-                    # Discord.py logging? unsure how to achieve this...
-                    print("ValueError (on_invite_delete)")
+            entry_found.pop(invite.code, None)
 
     # TODO: to verify redundancy - check if invites are available for
     # deleted channels
+    @commands.Cog.listener()
     async def on_guild_channel_delete(self, channel):
         pass
 
+    async def fetch_invites(self, guild):
+        try:
+            invites = await guild.invites()
+        except discord.HTTPException:
+            return None
+        else:
+            return {invite.code: invite for invite in invites}
+
+    @commands.Cog.listener()
     async def on_guild_join(self, guild):
         # (maybe) send event here
-        pass
+        invites = await self.fetch_invites(guild) or {}
+        self.cache[guild.id] = invites
 
     # can be used if not run during guild cache population
     # spoiler: it's not
@@ -94,10 +102,22 @@ class Invites(commands.Cog):
     # async def on_guild_remove(self, guild):
     #     pass
 
+    @commands.Cog.listener()
+    async def on_member_join_invite(self, member, invite):
+        print(f"{member} joined {member.guild} with invite {invite}")
+
+    @commands.Cog.listener()
     async def on_member_join(self, member):
-        # send event here
-        pass
+        invites = await self.fetch_invites(member.guild)
+        if invites is not None:
 
+            # we sort the invites to ensure we are comparing A.uses == A.uses
+            invites = sorted(invites.values(), key=lambda i: i.code)
+            cached = sorted(self.cache[member.guild.id].values(), key=lambda i: i.code)
 
-def setup(bot):
-    bot.add_cog(Invites(bot))
+            # zipping is the easiest way to compare each in order, and they should be the same size? if we do it properly
+            for old, new in zip(cached, invites):
+                if old.uses != new.uses:
+                    self.cache[member.guild.id][old.code] = new
+                    self.bot.dispatch("member_join_invite", member, new)
+                    break
