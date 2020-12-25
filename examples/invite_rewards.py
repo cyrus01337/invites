81509f4c60f51a12d2813b73f923112f79b584f7
cyrus01337 <cyrus21337@gmail.com>
2020-12-25 04:36:26 +0000

    Minor structure rewrite

--- examples/invite_rewards.py
+++ examples/invite_rewards.py
@@ -1,52 +1,37 @@
 """
-PEP8-complient cog showcasing invites by incorporating it into a basic
+PEP8-complient bot showcasing invites by incorporating it into a basic
 rewards system - once an invite has reached a certain number of uses,
 the creator of the invite will be rewarded
 """
-# cog.py
 import discord
 from discord.ext import commands
 
-
-class Invites(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-        self.thresholds = (10, 25, 50, 100)
-
-    @commands.Cog.listener()
-    async def on_invite_update(self, member, invite):
-        await self.bot.wait_for_invites()
-        print(f"{member} joined {member.guild} with invite {invite}")
-        can_send = member.guild.system_channel is not None
-
-        if invite.uses in self.thresholds and can_send:
-            try:
-                await member.guild.system_channel.send(
-                    f"**Congratulations** to {invite.inviter} for reaching the "
-                    f"**{invite.uses}** invite threshold! They will be "
-                    f"rewarded with **{1000*invite.uses:,}** shiny rocky-wocks!"
-                )
-            except discord.Forbidden:
-                print(f"[FAILED] {invite.code} @ {invite.uses} by "
-                      f"{invite.inviter}")
-
-
-def setup(bot):
-    bot.add_cog(Invites(bot))
-
-
-# bot.py
-from discord.ext import commands
-
 bot = commands.Bot(command_prefix="prefix ")
-bot.load_extension("invites")
-bot.load_extension("jishaku")
+bot.thresholds = (10, 25, 50, 100)
 
 
 @bot.event
 async def on_ready():
     print("Loaded", bot.user.name, end="\n\n")
+
+
+@bot.listen()
+async def on_invite_update(member, invite):
+    await bot.wait_for_invites()
+    print(f"{member} joined {member.guild} with invite {invite}")
+    can_send = member.guild.system_channel is not None
+
+    if invite.uses in bot.thresholds and can_send:
+        try:
+            # I am sorry that rocky-wocks was all that came to mind
+            await member.guild.system_channel.send(
+                f"**Congratulations** to {invite.inviter} for reaching the "
+                f"**{invite.uses}** invite threshold! They will be "
+                f"rewarded with **{1000*invite.uses:,}** shiny rocky-wocks!"
+            )
+        except discord.Forbidden:
+            print(f"[FAILED] {invite.code} @ {invite.uses} by "
+                  f"{invite.inviter}")
 
 
 # ?tag token - :^)
